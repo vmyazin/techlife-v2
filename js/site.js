c6c884375f0dede1b8206703fed65514f446524d
Vasily Myazin <vasily@automotivemastermind.com>
2019-01-13 11:52:08 -0500

    Add mustache templating; remove some hardcoded text

--- js/site.js
+++ js/site.js
@@ -1,5 +1,7 @@
 $(function() {
-  var xmlText;
+
+  var xmlText,
+      jsonEpisodeList;
 
   $.ajax({
     url: 'http://techlifepodcast.com/podcast-feed.xml',
@@ -10,24 +12,43 @@ $(function() {
       alert('Error loading XML document');
     },
     success: function(xmlText) {
-      // console.log(xmlText);
-      parseXML(xmlText);
+      parseXML2(xmlText);
     }
   });
 
+  window.showDetails = (e) => {
+    e.preventDefault();
+    
+  }
 
-  // $.get('http://techlifepodcast.com/podcast-feed.xml', function(data) {
-  //   xmlText = data;
-  // }).done(function() {
-  //   console.log(xmlText);
-  //   parseXML(xmlText);
-  // });  
+  function renderList(data) {
+    var mustache = require('mustache');
+    var xxx = [];
+    
+    const episodeList = data.rss.channel[0].item.map(episode => {
+      console.warn(episode);
+      
+      const episodeNumber = episode.title[0].split(":")[0];
+      // episode.example = true;
+      episode.numEpisode = episodeNumber.replace("#","");
+      episode.title = episode.title[0].replace(episodeNumber + ": ", "")
+      return episode;
+    });
 
-  function parseXML(file) {
-    var XMLParser = require('react-xml-parser');
-    var xml = new XMLParser().parseFromString(file);
-    console.log(xml);
-    console.log(xml.getElementsByTagName('item'));
+    var template = "{{#.}}<li><span class='episode-num'>№{{numEpisode}}</span> <a onclick='showDetails(event)' href='episodes/{{numEpisode}}'>{{title}}</a></li>{{/.}}";
+    var text = mustache.to_html(template, episodeList);
+    console.log(episodeList);
+
+    var list = document.getElementById('episode-list');
+    list.insertAdjacentHTML('beforeend', text);
+  }
+
+  function parseXML2(data) {
+    var parseString = require('xml2js').parseString;
+    parseString(data, function (err, w) {
+      renderList(w);
+      window.w = w;
+    });
   }
 });
 
